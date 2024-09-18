3f756955fe6768278e8911c6dc44d087843fe1a3
UmidIsanov <isanovumid8@gmail.com>
2024-09-18 13:50:00 +0500

    feat: user register api

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute");
 
 const app = express();
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
