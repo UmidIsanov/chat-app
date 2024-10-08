be83af10aac3ffa3025b543a8d244f7acf04ffc5
UmidIsanov <isanovumid8@gmail.com>
2024-10-08 10:45:51 +0500

    feat: add message controller

---
+++ server/Controllers/messageController.js
@@ -0,0 +1,35 @@
+const messageModel = require("../Models/messageModel");
+
+// createMessage
+// getMessages
+
+const createMessage = async (req, res) => {
+  const { chatId, senderId, text } = req.body;
+
+  const message = new messageModel({
+    chatId,
+    senderId,
+    text,
+  });
+
+  try {
+    const respone = await message.save();
+    res.status(200).json(respone);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+const getMessages = async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    const messages = await messageModel.find({ chatId });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.express = { createMessage, getMessages };
